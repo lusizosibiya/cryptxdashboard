25ddd94e790381e1cf1ac261030ded3c764ac0c3
Lusizo Sibiya <sibiyalusizo2003@gmail.com>
2026-09-21 21:28:24 +0200

    feat: add main dashboard layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Sidebar from "./components/layout/sidebar";
 import Header from "./components/layout/header";
+import Main from "./components/layout/main_content";
 
 function App() {
   return (
     <div className="flex">
       <Sidebar />
       <Header />
+      <Main/>
     </div>
   );
 }
---
+++ src/components/layout/main_content.jsx
@@ -0,0 +1,27 @@
+
+function Main() {
+    return (
+        <div>
+            {/* Top Section */}
+            <div className="flex gap-[20px]">
+                <div className="grid grid-cols-2 w-[396px] h-[326px] gap-x-[22px] gap-y-[30px]">
+
+                </div>
+                <div className="w-[394px] h-[326px]">
+
+                </div>
+            </div>
+            {/* Bottom Section */}
+            <div className="flex gap-[40px]">
+                <div className="w-[692px] h-[358px]">
+
+                </div>
+                <div className="w-[320px] h-[352px]">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
